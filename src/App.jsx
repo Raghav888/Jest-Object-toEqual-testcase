d07cbb5b60e41a9ee9f68a34e38fd7e33a8887d8
Raghav Bang <raghavbang2012@gmail.com>
2022-06-06 21:49:02 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1 className="app-header">tanaypratap's box</h1>
       <div className="app-body">Put your app body here</div>
     </div>
   );
